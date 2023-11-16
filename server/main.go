12610fc95a162783acee68f9b9dc11bbb3ca111a
Igor Becker <igor_nyu@hotmail.com>
2023-11-16 09:33:40 -0300

    yeah

--- server/main.go
+++ server/main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	// "server/src/infrastrutcture/database"
-	external_api "server/src/infrastrutcture/external-api"
-	"server/src/infrastrutcture/repositories"
+    "server/src/application/cmd"
 )
 
 func main() {
+    cmd.Exec()
     // dbConn, err := database.ConnectDb()
     //
     // if err != nil {
     //     log.Fatal("Could not connect to the Database")
     // }
-
-	base_url, err := external_api.NewApiUrl("https://ragnapi.com/api/v1/re-newal", "/monsters/", "/items/")
-
-	if err != nil {
-		log.Fatal("Not possible to create the base url")
-	}
-
-	fmt.Printf("init downloading files\n")
-    urls, err := base_url.CreateMonsterUrlRequest()
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    var monsterRepo repositories.MonsterRepository
-
-    external_api.GetUrlsAndCreateRecord(urls, monsterRepo)
 }
---
+++ server/src/application/cmd/cmd.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	external_api "server/src/infrastrutcture/external-api"
+	"server/src/infrastrutcture/repositories"
+)
+
+func Exec() {
+	base_url, err := external_api.NewApiUrl("https://ragnapi.com/api/v1/re-newal", "/monsters/", "/items/")
+
+	if err != nil {
+		log.Fatal("Not possible to create the base url")
+	}
+
+	fmt.Printf("init downloading files\n")
+    urls, err := base_url.CreateMonsterUrlRequest()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var monsterRepo repositories.MonsterRepository
+
+    external_api.GetUrlsAndCreateRecord(urls, monsterRepo)
+}
